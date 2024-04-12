0a54db85d976e1bb85b920e38b21badcabbd532e
Jim Sims <jim.sims@isilon.com>
2024-04-12 08:43:12 -0700

    typo fix

--- installcron.sh
+++ installcron.sh
@@ -1,5 +1,5 @@
 echo '# Every hour check to ensure isi_tiny_api is running
-0       *       *       *       *       root    /bin/bash /ifs/data/Isilon_Support/isitinyapi-main/run.sh' >> /etc/mcp/template/crontab && \
+0       *       *       *       *       root    /bin/bash /ifs/data/Isilon_Support/isitinyapi-main/run.sh' >> /etc/mcp/templates/crontab && \
 isi services -a isi_mcp disable && \
 isi services -a isi_mcp enable && \
 echo Success || echo Error
