afbd9c6e6b513d02042e2a5ba566b02b49db7904
Jim Sims <jim.sims@isilon.com>
2024-04-12 12:29:53 -0700

    fix

--- deployhttpdconf.sh
+++ deployhttpdconf.sh
@@ -1,3 +1,3 @@
 cp /ifs/data/Isilon_Support/isitinyapi-main/webui_httpd.conf /usr/local/apache2/conf/webui_httpd.conf && \
-isi services -a isi_webui disable && isi services -a isi_webui && enable && \
+isi services -a isi_webui disable && isi services -a isi_webui enable && \
 echo Success
